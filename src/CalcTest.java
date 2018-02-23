96deaec0b337c4c6893cc29533c7b57610f82dfd
Lucas Harris <harrislr@miamioh.edu>
2018-02-23 13:24:50 -0500

    Calc test

---
+++ src/CalcTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CalcTest {
+
+	@Test
+	public void intTest() {
+		assertEquals(Calc.divide(4,2), 2);
+	}
+	
+	@Test
+	public void notFloatTest() {
+		assertNotEquals(Calc.divide(4, 2), 2.0);
+	}
+	
+	@Test(expected = Exception.class)
+	public void divideByZeroTest() {
+		Calc.divide(1, 0);
+	}
+	
+	@Test
+	public void roundDownTest() {
+		assertEquals(Calc.divide(5, 2), 2);
+	}
+}
